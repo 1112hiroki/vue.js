ea325dfa7b2f2856db78328b3767513428fd4c2d
hiroki <1112hiroki.suzuki@gmail.com>
2020-02-15 19:08:37 +0900

    P74

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-// 動くものクラス を定義
-var Moveble = function(x, y) {
-  this.pos = {
-    x: x,
-    y: y
-  };
-  this.move = function(x,y){
-    this.pos.x += x;
-    this.pos.y += y;
-  };
-}
-
-// ボールオブジェクトを格納する空の配列を用意する
-var ball = [];
-
-// １００回繰り返し
-for (var i = 0; i<=100; i++) {
-// ボールオブジェクトのインスタンスを生成する
-  ball[i] = new Moveble(
-    Math.floor(Math.random()*window.innerWidth),
-    Math.floor(Math.random()*window.innerHeight)
-  );
-}
-
-// ボールをブラウザに描写する
-for (var i=0; i<=100; i++) {
-  document.write('<div class="ball" style="top:' + ball[i].pos.y + 'px;left:' + ball[i].pos.x + 'px;">●</div>');
-}
+var app = new Vue({
+  el: '#app',
+  data: {
+    year: (new Date()).getFullYear()
+  },
+  computed: {
+    isUrudoshi: function(){
+      if ((this.year%4 == 0) && (this.year%100 != 0) || (this.year%400 == 0)) {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  }
+});
