4702106644ac4755536e5e388ffda5ee1a40a67f
hiroki <1112hiroki.suzuki@gmail.com>
2020-02-15 18:05:51 +0900

    P48

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-var app = new Vue({
-  el:'#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
+// 動くものクラス を定義
+var Moveble = function(x, y) {
+  this.pos = {
+    x: x,
+    y: y
+  };
+  this.move = function(x,y){
+    this.pos.x += x;
+    this.pos.y += y;
+  };
+}
+
+// ボールオブジェクトを格納する空の配列を用意する
+var ball = [];
+
+// １００回繰り返し
+for (var i = 0; i<=100; i++) {
+// ボールオブジェクトのインスタンスを生成する
+  ball[i] = new Moveble(
+    Math.floor(Math.random()*window.innerWidth),
+    Math.floor(Math.random()*window.innerHeight)
+  );
+}
+
+// ボールをブラウザに描写する
+for (var i=0; i<=100; i++) {
+  document.write('<div class="ball" style="top:' + ball[i].pos.y + 'px;left:' + ball[i].pos.x + 'px;">●</div>');
+}
